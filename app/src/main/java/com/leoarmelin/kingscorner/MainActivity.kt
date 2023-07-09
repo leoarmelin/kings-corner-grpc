9b8deb30bca92e5a2af4d4770dd842f27c92c9d6
leoarmelin <leonardo99.armelin@gmail.com>
2023-07-09 09:34:32 -0300

    feat(game-screen): add screen to main activity for now

--- app/src/main/java/com/leoarmelin/kingscorner/MainActivity.kt
+++ app/src/main/java/com/leoarmelin/kingscorner/MainActivity.kt
@@ -3,35 +3,20 @@ package com.leoarmelin.kingscorner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
+import com.leoarmelin.kingscorner.ui.screens.GameScreen
 import com.leoarmelin.kingscorner.ui.theme.KingsCornerTheme
 import com.leoarmelin.kingscorner.viewmodels.MainViewModel
 
 class MainActivity : ComponentActivity() {
-
     private val mainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KingsCornerTheme {
-                // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    Button(onClick = { mainViewModel.createGame() }) {
-                        Text("Create")
-                    }
-
-                    Button(onClick = { mainViewModel.beginGame() }) {
-                        Text("Begin")
-                    }
-                }
+                GameScreen(
+                    mainViewModel = mainViewModel
+                )
             }
         }
     }
